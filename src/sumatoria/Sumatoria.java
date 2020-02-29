cda65d04f8839a3ada7a1ff5bcb6a87d50283fa6
Raxel21 <yisusparley@gmail.com>
2020-02-29 14:31:06 -0600

    Iniciando el proyecto

---
+++ src/sumatoria/Sumatoria.java
@@ -0,0 +1,16 @@
+package sumatoria;
+
+/**
+ *
+ * @author Raxel21
+ */
+public class Sumatoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
