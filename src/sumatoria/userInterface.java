a9d05401b4891cc43be3a35444019f4ea805ccfe
Raxel21 <yisusparley@gmail.com>
2020-02-29 14:42:41 -0600

    Añadí un evento click al boton

--- src/sumatoria/userInterface.java
+++ src/sumatoria/userInterface.java
@@ -39,6 +39,11 @@ public class userInterface extends javax.swing.JFrame {
         jLabel2.setText("Ingresa un número");
 
         jButton1.setText("Hallar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jLabel3.setText("La suma es");
 
@@ -83,6 +88,10 @@ public class userInterface extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
